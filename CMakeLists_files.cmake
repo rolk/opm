40380b88872c32df233a2143e9944b1966a05588
Roland Kaufmann <roland.kaufmann@uni.no>
2013-03-08 00:22:13 +0100

    List of sub-directories that contain projects to build

---
+++ CMakeLists_files.cmake
@@ -0,0 +1,11 @@
+# -*- mode: cmake; tab-width: 2; indent-tabs-mode: t; truncate-lines: t; compile-command: "cmake -Wdev" -*-
+# vim: set filetype=cmake autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap:
+
+# sub-directories that contain projects to build
+set (PROJECTS
+  opm-core
+  opm-polymer
+  dune-cornerpoint
+  opm-porsol
+  opm-upscaling
+  )
